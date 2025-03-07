9854b3ef04c3dc0d4b55254ac9cd41096ab5cf02
heimibanfan <heimibanfan@aliyun.com>
2025-03-07 23:31:10 +0800

    修复localdatetime序列化的问题

--- src/main/java/org/turbo/web/core/init/DefaultJacksonTurboServerInit.java
+++ src/main/java/org/turbo/web/core/init/DefaultJacksonTurboServerInit.java
@@ -2,8 +2,12 @@ package org.turbo.web.core.init;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.turbo.web.utils.common.BeanUtils;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * 默认的jackson初始化器
@@ -12,9 +16,8 @@ public class DefaultJacksonTurboServerInit extends JacksonTurboServerInit{
 
     @Override
     public void init(ObjectMapper objectMapper) {
-        objectMapper.registerModule(new JavaTimeModule());
-        String pattern = "yyyy-MM-dd HH:mm:ss";
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
-        objectMapper.setDateFormat(simpleDateFormat);
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        objectMapper.registerModule(javaTimeModule);
     }
 }
