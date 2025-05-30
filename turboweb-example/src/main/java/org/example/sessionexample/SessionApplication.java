4770f7c078bbd65e219c05750f6cfed527963a9a
黑米拌饭 <heimibanfan@aliyun.com>
2025-05-30 17:23:02 +0800

    引入黑洞垃圾回收器

--- turboweb-example/src/main/java/org/example/sessionexample/SessionApplication.java
+++ turboweb-example/src/main/java/org/example/sessionexample/SessionApplication.java
@@ -2,11 +2,13 @@ package org.example.sessionexample;
 
 import top.turboweb.core.server.StandardTurboWebServer;
 import top.turboweb.core.server.TurboWebServer;
+import top.turboweb.http.session.BackHoleSessionManager;
 
 public class SessionApplication {
 	public static void main(String[] args) {
 		TurboWebServer server = new StandardTurboWebServer(SessionApplication.class);
 		server.controllers(new UserController());
+		server.replaceSessionManager(new BackHoleSessionManager());
 		server.start();
 	}
 }
