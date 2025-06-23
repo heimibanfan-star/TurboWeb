d67b8606283aa7bb0bc81178271a5dacddb4bafd
黑米拌饭 <heimibanfan@aliyun.com>
2025-06-23 21:17:10 +0800

    中间件调用处理器添加对文件的释放

--- turboweb-http/src/main/java/top/turboweb/http/processor/MiddlewareInvokeProcessor.java
+++ turboweb-http/src/main/java/top/turboweb/http/processor/MiddlewareInvokeProcessor.java
@@ -4,6 +4,7 @@ import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.HttpResponse;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.codec.http.multipart.FileUpload;
 import top.turboweb.commons.lock.Locks;
 import top.turboweb.http.connect.ConnectSession;
 import top.turboweb.http.context.FullHttpContext;
@@ -18,6 +19,8 @@ import top.turboweb.http.session.DefaultHttpSession;
 import top.turboweb.http.session.HttpSession;
 import top.turboweb.http.session.SessionManagerHolder;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -54,34 +57,59 @@ public class MiddlewareInvokeProcessor extends Processor{
      * @return 响应对象
      */
     private HttpResponse executeMiddleware(FullHttpRequest fullHttpRequest, ConnectSession connectSession) {
-        // 构造响应对象
-        HttpInfoResponse httpInfoResponse = new HttpInfoResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
         // 封装请求对象
         HttpInfoRequest httpInfoRequest = HttpInfoRequestPackageHelper.packageRequest(fullHttpRequest);
-        // 初始化session
-        Cookies cookies = httpInfoRequest.getCookies();
-        String originSessionId = cookies.getCookie("JSESSIONID");
-        HttpSession httpSession = new DefaultHttpSession(sessionManagerHolder.getSessionManager(), originSessionId);
-        // 创建HttpContext对象
-        HttpContext context = new FullHttpContext(httpInfoRequest, httpSession, httpInfoResponse, connectSession);
-        // 执行中间件
-        Object result = chain.invoke(context);
-        HttpResponse response;
-        if (context.isWrite()) {
-            response = context.getResponse();
-        } else {
-            response = converter.convertor(result);
+        try {
+            // 构造响应对象
+            HttpInfoResponse httpInfoResponse = new HttpInfoResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+            // 初始化session
+            Cookies cookies = httpInfoRequest.getCookies();
+            String originSessionId = cookies.getCookie("JSESSIONID");
+            HttpSession httpSession = new DefaultHttpSession(sessionManagerHolder.getSessionManager(), originSessionId);
+            // 创建HttpContext对象
+            HttpContext context = new FullHttpContext(httpInfoRequest, httpSession, httpInfoResponse, connectSession);
+            // 执行中间件
+            Object result = chain.invoke(context);
+            HttpResponse response;
+            if (context.isWrite()) {
+                response = context.getResponse();
+            } else {
+                response = converter.convertor(result);
+            }
+            // 处理session
+            if (httpSession.sessionId() != null && (!Objects.equals(httpSession.sessionId(), originSessionId) || httpSession.pathIsUpdate())) {
+                response.headers().add("Set-Cookie", "JSESSIONID=" + httpSession.sessionId() + "; Path="+ httpSession.getPath() +"; HttpOnly");
+            }
+            // 续时
+            httpSession.expireAt();
+            // 处理响应结果
+            if (context.isWrite()) {
+                return context.getResponse();
+            }
+            return converter.convertor(result);
+        } finally {
+            releaseFiles(httpInfoRequest);
         }
-        // 处理session
-        if (httpSession.sessionId() != null && (!Objects.equals(httpSession.sessionId(), originSessionId) || httpSession.pathIsUpdate())) {
-            response.headers().add("Set-Cookie", "JSESSIONID=" + httpSession.sessionId() + "; Path="+ httpSession.getPath() +"; HttpOnly");
+    }
+
+
+    /**
+     * 释放文件上传对象
+     * @param httpInfoRequest 请求对象
+     */
+    private void releaseFiles(HttpInfoRequest httpInfoRequest) {
+        if (httpInfoRequest == null) {
+            return;
         }
-        // 续时
-        httpSession.expireAt();
-        // 处理响应结果
-        if (context.isWrite()) {
-            return context.getResponse();
+        Map<String, List<FileUpload>> fileUploads = httpInfoRequest.getContent().getFormFiles();
+        if (fileUploads != null) {
+            for (List<FileUpload> fileUploadList : fileUploads.values()) {
+                for (FileUpload fileUpload : fileUploadList) {
+                    if (fileUpload != null && fileUpload.refCnt() > 0) {
+                        fileUpload.release();
+                    }
+                }
+            }
         }
-        return converter.convertor(result);
     }
 }
