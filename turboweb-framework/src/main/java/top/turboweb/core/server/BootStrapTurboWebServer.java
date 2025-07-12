05e60bc0c1ecf7c7358086881e611a9c38da1f12
黑米拌饭 <heimibanfan@aliyun.com>
2025-07-12 16:05:25 +0800

    增加banner

--- turboweb-framework/src/main/java/top/turboweb/core/server/BootStrapTurboWebServer.java
+++ turboweb-framework/src/main/java/top/turboweb/core/server/BootStrapTurboWebServer.java
@@ -29,174 +29,189 @@ import java.util.function.Consumer;
 public class BootStrapTurboWebServer extends CoreTurboWebServer implements TurboWebServer {
 
 
-	private static final Logger log = LoggerFactory.getLogger(BootStrapTurboWebServer.class);
-	private HttpServerConfig serverConfig = new HttpServerConfig();
-	private final HttpSchedulerInitFactory httpSchedulerInitFactory;
-	private final HttpProtocolDispatcherInitFactory httpProtocolDispatcherInitFactory;
-	private final HttpClientInitializer httpClientInitializer;
-	private final CommonSourceInitializer commonSourceInitializer;
-	private final List<TurboWebListener> defaultListeners = new ArrayList<>(1);
-	private final List<TurboWebListener> customListeners = new ArrayList<>(1);
-	private boolean executeDefaultListener = true;
+    private static final Logger log = LoggerFactory.getLogger(BootStrapTurboWebServer.class);
+    private HttpServerConfig serverConfig = new HttpServerConfig();
+    private final HttpSchedulerInitFactory httpSchedulerInitFactory;
+    private final HttpProtocolDispatcherInitFactory httpProtocolDispatcherInitFactory;
+    private final HttpClientInitializer httpClientInitializer;
+    private final CommonSourceInitializer commonSourceInitializer;
+    private final List<TurboWebListener> defaultListeners = new ArrayList<>(1);
+    private final List<TurboWebListener> customListeners = new ArrayList<>(1);
+    private boolean executeDefaultListener = true;
 
-	{
-		httpSchedulerInitFactory = new HttpSchedulerInitFactory(this);
-		httpProtocolDispatcherInitFactory = new HttpProtocolDispatcherInitFactory(this);
-		httpClientInitializer = new DefaultHttpClientInitializer();
-		commonSourceInitializer = new DefaultCommonSourceInitializer();
-		defaultListeners.add(new DefaultJacksonTurboWebListener());
-	}
+    {
+        httpSchedulerInitFactory = new HttpSchedulerInitFactory(this);
+        httpProtocolDispatcherInitFactory = new HttpProtocolDispatcherInitFactory(this);
+        httpClientInitializer = new DefaultHttpClientInitializer();
+        commonSourceInitializer = new DefaultCommonSourceInitializer();
+        defaultListeners.add(new DefaultJacksonTurboWebListener());
+    }
 
-	public BootStrapTurboWebServer() {
-		this( 0);
-	}
+    public BootStrapTurboWebServer() {
+        this(0);
+    }
 
-	public BootStrapTurboWebServer(int ioThreadNum) {
-		super(ioThreadNum);
-	}
+    public BootStrapTurboWebServer(int ioThreadNum) {
+        super(ioThreadNum);
+    }
 
-	@Override
-	public HttpProtocolDispatcherBuilder protocol() {
-		return this.httpProtocolDispatcherInitFactory;
-	}
+    @Override
+    public HttpProtocolDispatcherBuilder protocol() {
+        return this.httpProtocolDispatcherInitFactory;
+    }
 
-	@Override
-	public HttpSchedulerInitBuilder http() {
-		return this.httpSchedulerInitFactory;
-	}
+    @Override
+    public HttpSchedulerInitBuilder http() {
+        return this.httpSchedulerInitFactory;
+    }
 
-	@Override
-	public TurboWebServer configServer(Consumer<HttpServerConfig> consumer) {
-		Objects.requireNonNull(consumer, "consumer can not be null");
-		consumer.accept(this.serverConfig);
-		return this;
-	}
+    @Override
+    public TurboWebServer configServer(Consumer<HttpServerConfig> consumer) {
+        Objects.requireNonNull(consumer, "consumer can not be null");
+        consumer.accept(this.serverConfig);
+        return this;
+    }
 
-	@Override
-	public TurboWebServer replaceServerConfig(HttpServerConfig httpServerConfig) {
-		Objects.requireNonNull(httpServerConfig, "httpServerConfig can not be null");
-		this.serverConfig = httpServerConfig;
-		return this;
-	}
+    @Override
+    public TurboWebServer replaceServerConfig(HttpServerConfig httpServerConfig) {
+        Objects.requireNonNull(httpServerConfig, "httpServerConfig can not be null");
+        this.serverConfig = httpServerConfig;
+        return this;
+    }
 
-	@Override
-	public TurboWebServer configClient(Consumer<HttpClientConfig> consumer) {
-		Objects.requireNonNull(consumer, "consumer can not be null");
-		httpClientInitializer.config(consumer);
-		return this;
-	}
+    @Override
+    public TurboWebServer configClient(Consumer<HttpClientConfig> consumer) {
+        Objects.requireNonNull(consumer, "consumer can not be null");
+        httpClientInitializer.config(consumer);
+        return this;
+    }
 
-	@Override
-	public TurboWebServer replaceClientConfig(HttpClientConfig httpClientConfig) {
-		Objects.requireNonNull(httpClientConfig, "httpClientConfig can not be null");
-		this.httpClientInitializer.replaceConfig(httpClientConfig);
-		return this;
-	}
+    @Override
+    public TurboWebServer replaceClientConfig(HttpClientConfig httpClientConfig) {
+        Objects.requireNonNull(httpClientConfig, "httpClientConfig can not be null");
+        this.httpClientInitializer.replaceConfig(httpClientConfig);
+        return this;
+    }
 
 
-	@Override
-	public TurboWebServer executeDefaultListener(boolean flag) {
-		this.executeDefaultListener = flag;
-		return this;
-	}
+    @Override
+    public TurboWebServer executeDefaultListener(boolean flag) {
+        this.executeDefaultListener = flag;
+        return this;
+    }
 
-	@Override
-	public TurboWebServer listeners(TurboWebListener... listeners) {
-		customListeners.addAll(List.of(listeners));
-		return this;
-	}
+    @Override
+    public TurboWebServer listeners(TurboWebListener... listeners) {
+        customListeners.addAll(List.of(listeners));
+        return this;
+    }
 
-	@Override
-	public ChannelFuture start() {
-		return start(8080);
-	}
+    @Override
+    public ChannelFuture start() {
+        return start(8080);
+    }
 
-	@Override
-	public ChannelFuture start(int port) {
-		return start("0.0.0.0", port);
-	}
+    @Override
+    public ChannelFuture start(int port) {
+        return start("0.0.0.0", port);
+    }
 
-	@Override
-	public ChannelFuture start(String host, int port) {
-		long start = System.currentTimeMillis();
-		executeListenerBeforeInit();
-		init();
-		ChannelFuture channelFuture = startServer(host, port);
-		channelFuture.addListener(future -> {
-			if (future.isSuccess()) {
-				executeListenerAfterServerStart();
-				long time = System.currentTimeMillis() - start;
-				log.info("TurboWebServer start on: http://{}:{}, time: {}ms", host, port, time);
-			} else {
-				log.error("TurboWebServer start failed: {}\n", future.cause().getMessage(), future.cause());
-			}
+    @Override
+    public ChannelFuture start(String host, int port) {
+        printBanner();
+        long start = System.currentTimeMillis();
+        executeListenerBeforeInit();
+        init();
+        ChannelFuture channelFuture = startServer(host, port);
+        channelFuture.addListener(future -> {
+            if (future.isSuccess()) {
+                executeListenerAfterServerStart();
+                long time = System.currentTimeMillis() - start;
+                log.info("TurboWebServer start on: http://{}:{}, time: {}ms", host, port, time);
+            } else {
+                log.error("TurboWebServer start failed: {}\n", future.cause().getMessage(), future.cause());
+            }
 
-		});
-		return channelFuture;
-	}
+        });
+        return channelFuture;
+    }
 
-	/**
-	 * 初始化
-	 */
-	private void init() {
-		// 初始化公共资源
-		commonSourceInitializer.init(serverConfig);
-		// 初始化http客户端
-		httpClientInitializer.init(workers());
-		// 创建http调度器
-		HttpScheduler httpScheduler = httpSchedulerInitFactory.createHttpScheduler(serverConfig);
-		// 创建http协议分发器
-		HttpProtocolDispatcher httpProtocolDispatcher = httpProtocolDispatcherInitFactory.createDispatcher(httpScheduler);
-		initPipeline(httpProtocolDispatcher, serverConfig.getMaxContentLength());
-	}
+    /**
+     * 初始化
+     */
+    private void init() {
+        // 初始化公共资源
+        commonSourceInitializer.init(serverConfig);
+        // 初始化http客户端
+        httpClientInitializer.init(workers());
+        // 创建http调度器
+        HttpScheduler httpScheduler = httpSchedulerInitFactory.createHttpScheduler(serverConfig);
+        // 创建http协议分发器
+        HttpProtocolDispatcher httpProtocolDispatcher = httpProtocolDispatcherInitFactory.createDispatcher(httpScheduler);
+        initPipeline(httpProtocolDispatcher, serverConfig.getMaxContentLength());
+    }
 
-	/**
-	 * 执行监听器
-	 */
-	private void executeListenerBeforeInit() {
-		if (executeDefaultListener) {
-			for (TurboWebListener turboWebListener : defaultListeners) {
-				turboWebListener.beforeServerInit();
-			}
-		}
-		for (TurboWebListener turboWebListener : customListeners) {
-			turboWebListener.beforeServerInit();
-		}
-		log.info("TurboWeb初始化前置监听器方法执行完成");
-	}
+    public static void printBanner() {
+        String banner = """
+                 _______         _       __          __  _    \s
+                |__   __|       | |      \\ \\        / / | |   \s
+                   | |_   _ _ __| |__   __\\ \\  /\\  / /__| |__ \s
+                   | | | | | '__| '_ \\ / _ \\ \\/  \\/ / _ \\ '_ \\\s
+                   | | |_| | |  | |_) | (_) \\  /\\  /  __/ |_) |
+                   |_|\\__,_|_|  |_.__/ \\___/ \\/  \\/ \\___|_.__/\s
+                                                              \s
+                :: TurboWeb - Lightweight ⚙ Turbocharged ⚡ Web Framework ::
+                :: Powered by Virtual Threads + Netty, Built for Speed & Simplicity ::
+                """;
+        System.out.println(banner);
+    }
 
-	/**
-	 * 执行监听器
-	 */
-	private void executeListenerAfterServerStart() {
-		if (executeDefaultListener) {
-			for (TurboWebListener turboWebListener : defaultListeners) {
-				turboWebListener.afterServerStart();
-			}
-		}
-		for (TurboWebListener turboWebListener : customListeners) {
-			turboWebListener.afterServerStart();
-		}
-		log.info("TurboWeb启动后监听器方法执行完成");
-	}
+    /**
+     * 执行监听器
+     */
+    private void executeListenerBeforeInit() {
+        if (executeDefaultListener) {
+            for (TurboWebListener turboWebListener : defaultListeners) {
+                turboWebListener.beforeServerInit();
+            }
+        }
+        for (TurboWebListener turboWebListener : customListeners) {
+            turboWebListener.beforeServerInit();
+        }
+        log.info("TurboWeb初始化前置监听器方法执行完成");
+    }
 
-	/**
-	 * 创建TurboWebServer
-	 *
-	 * @param mainClass 主类
-	 * @return TurboWebServer
-	 */
-	public static TurboWebServer create() {
-		return new BootStrapTurboWebServer();
-	}
+    /**
+     * 执行监听器
+     */
+    private void executeListenerAfterServerStart() {
+        if (executeDefaultListener) {
+            for (TurboWebListener turboWebListener : defaultListeners) {
+                turboWebListener.afterServerStart();
+            }
+        }
+        for (TurboWebListener turboWebListener : customListeners) {
+            turboWebListener.afterServerStart();
+        }
+        log.info("TurboWeb启动后监听器方法执行完成");
+    }
 
-	/**
-	 * 创建TurboWebServer
-	 *
-	 * @param ioThreadNum IO线程数
-	 * @return TurboWebServer
-	 */
-	public static TurboWebServer create(int ioThreadNum) {
-		return new BootStrapTurboWebServer(ioThreadNum);
-	}
+    /**
+     * 创建TurboWebServer
+     *
+     * @return TurboWebServer
+     */
+    public static TurboWebServer create() {
+        return new BootStrapTurboWebServer();
+    }
+
+    /**
+     * 创建TurboWebServer
+     *
+     * @param ioThreadNum IO线程数
+     * @return TurboWebServer
+     */
+    public static TurboWebServer create(int ioThreadNum) {
+        return new BootStrapTurboWebServer(ioThreadNum);
+    }
 }
