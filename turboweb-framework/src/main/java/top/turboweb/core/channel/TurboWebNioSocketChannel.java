30d224a7bc60d1dbb82dae129d47b1dfa114607d
黑米拌饭 <heimibanfan@aliyun.com>
2025-07-31 23:38:09 +0800

    优化零拷贝底层的实现

--- turboweb-framework/src/main/java/top/turboweb/core/channel/TurboWebNioSocketChannel.java
+++ turboweb-framework/src/main/java/top/turboweb/core/channel/TurboWebNioSocketChannel.java
@@ -3,19 +3,21 @@ package top.turboweb.core.channel;
 import io.netty.channel.*;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
+import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.locks.ReentrantLock;
 
+import static io.netty.channel.internal.ChannelUtils.MAX_BYTES_PER_GATHERING_WRITE_ATTEMPTED_LOW_THRESHOLD;
+
 
 public class TurboWebNioSocketChannel extends NioSocketChannel {
 
     private final ExecutorService zeroCopyPool;
-    private volatile boolean isSuspend = false;
-    private final ReentrantLock lock = new ReentrantLock();
-    private final Queue<SuspendTask> suspendTasks = new ConcurrentLinkedQueue<>();
+    private volatile boolean isWriting = false;
+    private int maxBytesPerGatheringWrite = Integer.MAX_VALUE;
 
     private record SuspendTask(Object task, ChannelPromise channelFuture) {
     }
@@ -25,114 +27,102 @@ public class TurboWebNioSocketChannel extends NioSocketChannel {
         this.zeroCopyPool = pool;
     }
 
-    @Override
-    public ChannelFuture writeAndFlush(Object msg) {
-        return this.writeAndFlush(msg, new DefaultChannelPromise(this));
-    }
-
-    @Override
-    public ChannelFuture writeAndFlush(Object msg, ChannelPromise promise) {
-        lock.lock();
-        try {
-            // 判断当前连接是否被挂起
-            if (isSuspend) {
-                suspendTasks.add(new SuspendTask(msg, promise));
-                return promise;
-            } else if (msg instanceof FileRegion) {
-                // 挂起当前连接
-                isSuspend = true;
-                // 执行零拷贝
-                return super.writeAndFlush(msg, promise);
+    private void adjustMaxBytesPerGatheringWrite(int attempted, int written, int oldMaxBytesPerGatheringWrite) {
+        // By default we track the SO_SNDBUF when ever it is explicitly set. However some OSes may dynamically change
+        // SO_SNDBUF (and other characteristics that determine how much data can be written at once) so we should try
+        // make a best effort to adjust as OS behavior changes.
+        if (attempted == written) {
+            if (attempted << 1 > oldMaxBytesPerGatheringWrite) {
+                this.maxBytesPerGatheringWrite = attempted << 1;
             }
-        } finally {
-            lock.unlock();
+        } else if (attempted > MAX_BYTES_PER_GATHERING_WRITE_ATTEMPTED_LOW_THRESHOLD && written < attempted >>> 1) {
+            this.maxBytesPerGatheringWrite = attempted >>> 1;
         }
-        return super.writeAndFlush(msg, promise);
-    }
-
-    @Override
-    public ChannelFuture write(Object msg, ChannelPromise promise) {
-        lock.lock();
-        try {
-            if (isSuspend) {
-                suspendTasks.add(new SuspendTask(msg, promise));
-                return promise;
-            } else if (msg instanceof FileRegion) {
-                // 挂起当前连接
-                isSuspend = true;
-                // 执行零拷贝
-                return super.write(msg);
-            }
-        } finally {
-            lock.unlock();
-        }
-        return super.write(msg, promise);
-    }
-
-    @Override
-    public ChannelFuture write(Object msg) {
-        return this.write(msg, new DefaultChannelPromise(this));
-    }
-
-    @Override
-    public Channel flush() {
-        lock.lock();
-        try {
-            if (!isSuspend) {
-                return super.flush();
-            }
-        } finally {
-            lock.unlock();
-        }
-        return this;
     }
 
     @Override
     protected void doWrite(ChannelOutboundBuffer in) throws Exception {
-        // 判断当前任务是否是零拷贝任务
-        Object current = in.current();
-        if (current instanceof FileRegion) {
-            zeroCopyPool.execute(() -> {
-                try {
-                    super.doWrite(in);
-                } catch (Exception e) {
-                    this.close();
-                } finally {
-                    // 恢复剩余的任务
-                    activeTasks();
-                }
-            });
-        } else {
-            super.doWrite(in);
+        if (isWriting) {
+            incompleteWrite(false);
+            return;
         }
-    }
+        SocketChannel ch = javaChannel();
+        int writeSpinCount = config().getWriteSpinCount();
+        do {
+            if (in.isEmpty()) {
+                // All written so clear OP_WRITE
+                clearOpWrite();
+                // Directly return here so incompleteWrite(...) is not called.
+                return;
+            }
 
-    private void activeTasks() {
-        Thread.ofVirtual().start(() -> {
-           for(;;) {
-               lock.lock();
-               try {
-                   if (suspendTasks.isEmpty()) {
-                       isSuspend = false;
-                       break;
-                   }
-               } finally {
-                   lock.unlock();
-               }
-               SuspendTask task = suspendTasks.poll();
-               if (task != null) {
-                   try {
-                       Object msg = task.task;
-                       if (msg instanceof FileRegion) {
-                           super.writeAndFlush(msg, task.channelFuture);
-                           break;
-                       } else {
-                           super.writeAndFlush(msg, task.channelFuture);
-                       }
-                   } catch (Exception ignore) {
-                   }
-               }
-           }
-        });
+            // Ensure the pending writes are made of ByteBufs only.
+            int maxBytesPerGatheringWrite = this.maxBytesPerGatheringWrite;
+            ByteBuffer[] nioBuffers = in.nioBuffers(1024, maxBytesPerGatheringWrite);
+            int nioBufferCnt = in.nioBufferCount();
+
+            // Always use nioBuffers() to workaround data-corruption.
+            // See https://github.com/netty/netty/issues/2761
+            switch (nioBufferCnt) {
+                case 0:
+                    // We have something else beside ByteBuffers to write so fallback to normal writes.
+                    // 判断当前写入是否是零拷贝
+                    Object current = in.current();
+                    if (current instanceof FileRegion) {
+                        isWriting = true;
+                        zeroCopyPool.execute(() -> {
+                            try {
+                                doWrite0(in);
+                            } catch (Exception e) {
+                                this.close();
+                            } finally {
+                                isWriting = false;
+                                // 刷新缓冲区
+                                incompleteWrite(true);
+                                this.flush();
+                            }
+                        });
+                        incompleteWrite(false);
+                        return;
+                    }
+                    writeSpinCount -= doWrite0(in);
+                    break;
+                case 1: {
+                    // Only one ByteBuf so use non-gathering write
+                    // Zero length buffers are not added to nioBuffers by ChannelOutboundBuffer, so there is no need
+                    // to check if the total size of all the buffers is non-zero.
+                    ByteBuffer buffer = nioBuffers[0];
+                    int attemptedBytes = buffer.remaining();
+                    final int localWrittenBytes = ch.write(buffer);
+                    if (localWrittenBytes <= 0) {
+                        incompleteWrite(true);
+                        return;
+                    }
+                    adjustMaxBytesPerGatheringWrite(attemptedBytes, localWrittenBytes, maxBytesPerGatheringWrite);
+                    in.removeBytes(localWrittenBytes);
+                    --writeSpinCount;
+                    break;
+                }
+                default: {
+                    // Zero length buffers are not added to nioBuffers by ChannelOutboundBuffer, so there is no need
+                    // to check if the total size of all the buffers is non-zero.
+                    // We limit the max amount to int above so cast is safe
+                    long attemptedBytes = in.nioBufferSize();
+                    final long localWrittenBytes = ch.write(nioBuffers, 0, nioBufferCnt);
+                    if (localWrittenBytes <= 0) {
+                        incompleteWrite(true);
+                        return;
+                    }
+                    // Casting to int is safe because we limit the total amount of data in the nioBuffers to int above.
+                    adjustMaxBytesPerGatheringWrite((int) attemptedBytes, (int) localWrittenBytes,
+                            maxBytesPerGatheringWrite);
+                    in.removeBytes(localWrittenBytes);
+                    --writeSpinCount;
+                    break;
+                }
+            }
+        } while (writeSpinCount > 0);
+
+        incompleteWrite(writeSpinCount < 0);
     }
 }
