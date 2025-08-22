a81b8d35d104711d7acf6434cd9b8b7ff71ff145
黑米拌饭 <heimibanfan@aliyun.com>
2025-08-22 16:03:01 +0800

    除了GET和HEAD所有请求方式均可支持请求体

--- turboweb-http-server/src/main/java/top/turboweb/http/request/HttpInfoRequestPackageHelper.java
+++ turboweb-http-server/src/main/java/top/turboweb/http/request/HttpInfoRequestPackageHelper.java
@@ -49,15 +49,15 @@ public class HttpInfoRequestPackageHelper {
         HttpContent content = null;
         // 获取请求方式
         String method = request.method().name();
-        if (HttpMethod.POST.name().equals(method) || HttpMethod.PUT.name().equals(method) || HttpMethod.PATCH.name().equals(method)) {
+        if (HttpMethod.GET.name().equals(method) || HttpMethod.HEAD.name().equals(method)) {
+            content = HttpContent.empty();
+        } else {
             if (request.headers().get(HttpHeaderNames.CONTENT_TYPE) != null) {
                 // 只有当请求方式为post、put、patch时，并且请求体不为空时，才进行解析
                 content = parseBodyInfo(request);
             } else {
                 content = HttpContent.empty();
             }
-        } else {
-            content = HttpContent.empty();
         }
         return new HttpInfoRequest(request, queryParams, content);
     }
