403da42c0df05af7086099074adf4cff61560d02
黑米拌饭 <heimibanfan@aliyun.com>
2025-09-27 20:54:29 +0800

    删除RPC模块

---
+++ turboweb-commons/src/main/java/top/turboweb/commons/exception/TurboRPCException.java
@@ -0,0 +1,7 @@
+package top.turboweb.commons.exception;
+
+public class TurboRPCException extends RuntimeException {
+    public TurboRPCException(String message) {
+        super(message);
+    }
+}
--- turboweb-rpc/src/test/java/top/heimi/rpc/UserController.java
+++
@@ -1,9 +0,0 @@
-package top.heimi.rpc;
-
-/**
- * TODO
- */
-public interface UserController {
-
-    void show();
-}
