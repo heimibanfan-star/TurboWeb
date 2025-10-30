1da045f19775bae41522b4319186e64418d6dca2
黑米拌饭 <heimibanfan@aliyun.com>
2025-10-30 11:55:24 +0800

    简化http2.0与http1.1的适配层

--- turboweb-framework/src/main/java/top/turboweb/core/handler/Http2FrameAdaptorHandler.java
+++ turboweb-framework/src/main/java/top/turboweb/core/handler/Http2FrameAdaptorHandler.java
@@ -2,19 +2,12 @@ package top.turboweb.core.handler;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.CompositeByteBuf;
-import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPromise;
 import io.netty.handler.codec.http.*;
 import io.netty.handler.codec.http2.*;
-import org.reactivestreams.Subscription;
-import reactor.core.publisher.BaseSubscriber;
-import reactor.core.publisher.Flux;
 import top.turboweb.commons.config.GlobalConfig;
-import top.turboweb.commons.exception.TurboFileException;
-import top.turboweb.commons.exception.TurboResponseException;
-
 import java.util.LinkedList;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -46,16 +39,6 @@ public class Http2FrameAdaptorHandler extends ChannelDuplexHandler {
      * 响应写入状态标识，避免重复写入
      */
     private final AtomicBoolean writing = new AtomicBoolean();
-
-    /**
-     * 单个 HTTP/2 流的最大数据大小，用于分块发送
-     */
-    private int frameSize = DEFAULT_MAX_FRAME_SIZE;
-
-    /**
-     * 默认的 HTTP/2 帧大小
-     */
-    private static final int DEFAULT_MAX_FRAME_SIZE = 16 * 1024;
 
     /**
      * <h2>读取通道数据</h2>
@@ -126,7 +109,6 @@ public class Http2FrameAdaptorHandler extends ChannelDuplexHandler {
      * <h2>写出通道数据</h2>
      * <p>
      * 将 FullHttpResponse 或 HttpContent 转换为 HTTP/2 Headers/Data 帧发送。
-     * 支持自动拆分超过 {@link #frameSize} 的数据。
      * </p>
      *
      * @param ctx     通道上下文
@@ -164,7 +146,7 @@ public class Http2FrameAdaptorHandler extends ChannelDuplexHandler {
                         }
                     });
                 } else {
-                    ctx.write(http2HeadersFrame, promise)
+                    ctx.writeAndFlush(http2HeadersFrame, promise)
                             .addListener(future -> {
                                 if (future.isSuccess()) {
                                     writing.set(true);
@@ -187,70 +169,12 @@ public class Http2FrameAdaptorHandler extends ChannelDuplexHandler {
 
     /**
      * <h2>写入 HTTP/2 数据帧</h2>
-     * <p>
-     * 根据 {@link #frameSize} 将 ByteBuf 拆分为多个 DataFrame 分块发送，最后一帧根据 endStream 参数决定是否结束流。
-     * </p>
-     *
      * @param ctx       通道上下文
      * @param byteBuf   待写入的数据
      * @param endStream 是否为流结束帧
      */
     private void writeDateFrame(ChannelHandlerContext ctx, ByteBuf byteBuf, boolean endStream, ChannelPromise promise) {
-        final int frameSize = this.frameSize;
-        if (byteBuf.readableBytes() > frameSize) {
-            Flux.<ByteBuf, Integer>generate(byteBuf::readableBytes, (readable, sink) -> {
-                        // 判断是否超过分块大小
-                        if (readable > frameSize) {
-                            // 读取分块
-                            ByteBuf chunk = byteBuf.readRetainedSlice(frameSize);
-                            sink.next(chunk);
-                        } else {
-                            // 发送完整的数据
-                            sink.next(byteBuf);
-                            sink.complete();
-                        }
-                        return byteBuf.readableBytes();
-                    })
-                    .subscribe(new BaseSubscriber<ByteBuf>() {
-
-                        @Override
-                        protected void hookOnSubscribe(Subscription subscription) {
-                            request(1);
-                        }
-
-                        @Override
-                        protected void hookOnNext(ByteBuf value) {
-                            ctx.writeAndFlush(new DefaultHttp2DataFrame(value, false))
-                                    .addListener(future -> {
-                                        if (!future.isSuccess()) {
-                                            promise.tryFailure(future.cause());
-                                            ctx.close();
-                                            cancel();
-                                        } else {
-                                            request(1);
-                                        }
-                                    });
-                        }
-
-                        @Override
-                        protected void hookOnCancel() {
-                            promise.tryFailure(new TurboResponseException("frame stream is cancel"));
-                        }
-
-                        @Override
-                        protected void hookOnError(Throwable throwable) {
-                            promise.tryFailure(throwable);
-                        }
-
-                        @Override
-                        protected void hookOnComplete() {
-                            // 发送一个结束的空帧
-                            ctx.writeAndFlush(new DefaultHttp2DataFrame(Unpooled.EMPTY_BUFFER, endStream), promise);
-                        }
-                    });
-        } else {
-            // 写入完整的分块
-            ctx.writeAndFlush(new DefaultHttp2DataFrame(byteBuf, endStream), promise);
-        }
+        Http2DataFrame http2DataFrame = new DefaultHttp2DataFrame(byteBuf, endStream);
+        ctx.writeAndFlush(http2DataFrame, promise);
     }
 }
