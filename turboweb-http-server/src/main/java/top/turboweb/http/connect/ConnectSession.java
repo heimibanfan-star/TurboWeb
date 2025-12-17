d07cccbf70681755b3b06e5b87ea2a3b10047f2e
黑米拌饭 <heimibanfan@aliyun.com>
2025-12-17 17:46:42 +0800

    优化ConnectSession提供地址的获取API

--- turboweb-http-server/src/main/java/top/turboweb/http/connect/ConnectSession.java
+++ turboweb-http-server/src/main/java/top/turboweb/http/connect/ConnectSession.java
@@ -1,62 +1,45 @@
 package top.turboweb.http.connect;
 
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
-import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
-import io.netty.handler.codec.http.*;
-import io.netty.util.CharsetUtil;
+
+import java.net.SocketAddress;
 
 /**
  * sse的回话对象
  */
-public class ConnectSession {
-    protected final Channel channel;
-
-    public ConnectSession(Channel channel) {
-        this.channel = channel;
-    }
+public interface ConnectSession {
 
     /**
      * 向浏览器写入内容
      *
      * @param message 消息
      */
-    public ChannelFuture send(String message) {
-        ByteBuf buf = Unpooled.copiedBuffer(message, CharsetUtil.UTF_8);
-        DefaultHttpContent content = new DefaultHttpContent(buf); // 发送 chunked 数据
-        return channel.writeAndFlush(content);
-    }
+    ChannelFuture send(String message);
 
     /**
      * 当连接关闭时触发的回调
      *
      * @param runnable 回调
      */
-    public void closeListener(Runnable runnable) {
-//        promise.addListener(future -> {
-//            runnable.run();
-//        });
-        channel.closeFuture().addListener(future -> {
-            runnable.run();
-        });
-    }
+    void closeListener(Runnable runnable);
 
     /**
      * 关闭连接
      */
-    public void close() {
-        channel.eventLoop().execute(() -> {
-            if (!channel.isActive()) {
-                return;
-            }
-            // 刷新数据
-            channel.flush();
-            // 发送结束信号
-            channel.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT).addListener(future -> {
-                channel.close();
-            });
-        });
-    }
+    void close();
+
+    /**
+     * 获取远程地址
+     *
+     * @return 远程地址
+     */
+    SocketAddress getRemoteAddress();
+
+    /**
+     * 获取本地地址
+     *
+     * @return 本地地址
+     */
+    SocketAddress getLocalAddress();
 
 }
--- turboweb-http-server/src/main/java/top/turboweb/http/connect/InternalConnectSession.java
+++ turboweb-http-server/src/main/java/top/turboweb/http/connect/InternalConnectSession.java
@@ -1,14 +1,23 @@
 package top.turboweb.http.connect;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoop;
+import io.netty.handler.codec.http.DefaultHttpContent;
+import io.netty.handler.codec.http.LastHttpContent;
+import io.netty.util.CharsetUtil;
 
+import java.net.SocketAddress;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * 服务器内部使用的连接会话
  */
-public class InternalConnectSession extends ConnectSession{
+public class InternalConnectSession implements ConnectSession{
+
+    protected final Channel channel;
 
     /**
      * 每个channel拥有自己的一把锁，保护对该channel的写入原子性
@@ -16,7 +25,7 @@ public class InternalConnectSession extends ConnectSession{
     public final ReentrantLock channelLock = new ReentrantLock();
 
     public InternalConnectSession(Channel channel) {
-        super(channel);
+        this.channel = channel;
     }
 
     public Channel getChannel() {
@@ -26,4 +35,44 @@ public class InternalConnectSession extends ConnectSession{
     public EventLoop getExecutor() {
         return channel.eventLoop();
     }
+
+    @Override
+    public ChannelFuture send(String message) {
+        ByteBuf buf = Unpooled.copiedBuffer(message, CharsetUtil.UTF_8);
+        DefaultHttpContent content = new DefaultHttpContent(buf); // 发送 chunked 数据
+        return channel.writeAndFlush(content);
+    }
+
+    @Override
+    public void closeListener(Runnable runnable) {
+        channel.closeFuture().addListener(future -> {
+            runnable.run();
+        });
+    }
+
+    @Override
+    public void close() {
+        channel.eventLoop().execute(() -> {
+            if (!channel.isActive()) {
+                return;
+            }
+            // 刷新数据
+            channel.flush();
+            // 发送结束信号
+            channel.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT).addListener(future -> {
+                channel.close();
+            });
+        });
+    }
+
+    @Override
+    public SocketAddress getRemoteAddress() {
+        return channel.remoteAddress();
+    }
+
+    @Override
+    public SocketAddress getLocalAddress() {
+        return channel.localAddress();
+    }
+
 }
